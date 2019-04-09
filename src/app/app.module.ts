a5fcd12b4311098ca64d7220a62d9b10f2f0311f
Nick Visalli <nvisal1@students.towson.edu>
2019-04-09 15:52:53 -0400

    scaffold pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule, MatInputModule} from '@angular/material';
+import { RegisterComponent } from './authentication/pages/register/register.component';
+import { UserComponent } from './profile/pages/user/user.component';
+import { OtherComponent } from './profile/pages/other/other.component';
+import { BrowseComponent } from './browse/pages/browse/browse.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
+    OtherComponent,
+    BrowseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
