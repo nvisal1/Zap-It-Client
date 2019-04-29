9a08a06ec9f1477d888dfa33d42433cd6433ce0b
Nick Visalli <nvisal1@students.towson.edu>
2019-04-29 18:19:32 -0400

    A simple form validation to registration form

--- src/app/authentication/components/registration-form/registration-form.component.ts
+++ src/app/authentication/components/registration-form/registration-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationFormComponent implements OnInit {
 
+  form = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get username() {
+    return this.form.get('username');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
   }
 
 }
